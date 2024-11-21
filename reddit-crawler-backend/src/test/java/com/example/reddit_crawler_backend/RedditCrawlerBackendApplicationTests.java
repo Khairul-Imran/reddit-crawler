e934feff8361cfcd51945cb709930c7d73764eb1
Khairul <Khairulimran6810@gmail.com>
2024-11-21 23:58:13 +0800

    first commit

---
+++ reddit-crawler-backend/src/test/java/com/example/reddit_crawler_backend/RedditCrawlerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reddit_crawler_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCrawlerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
