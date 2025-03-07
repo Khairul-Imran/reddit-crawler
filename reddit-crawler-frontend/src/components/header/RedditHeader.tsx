e157148b1e868161dccd8c760b250d39a61db3ff
Khairul <Khairulimran6810@gmail.com>
2025-03-07 14:30:03 +0800

    Testing jenkins build.

--- reddit-crawler-frontend/src/components/header/RedditHeader.tsx
+++ reddit-crawler-frontend/src/components/header/RedditHeader.tsx
@@ -4,7 +4,7 @@ function RedditHeader(): JSX.Element {
             <div className="container mx-auto px-4 py-12">
                 <div className="text-center">
                     <h1 className="text-5xl font-extrabold text-white mb-4">
-                        Reddit Top 20 (Hello Jenkins!!)
+                        Reddit Top 20 (Done with first JenkinsPipeline!!!)
                     </h1>
                     <p className="text-lg text-white/90 max-w-2xl mx-auto leading-relaxed">
                         Check the top 20 posts from your favourite subreddit in the last 24 hours!
